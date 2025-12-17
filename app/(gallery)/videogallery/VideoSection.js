bd3e1b2642ed4f8ab071ab4da0def726d5224d45
AforAashish999 <aashishgharti108@gmail.com>
2025-12-17 18:10:29 +0545

    feat(VideoGallery): add video section

---
+++ app/(gallery)/videogallery/VideoSection.js
@@ -0,0 +1,28 @@
+import styles from "./Video-Section.module.css";
+
+export default function VideoSection() {
+    return (
+        <div className={styles.video} >
+            <div className={styles.video__item} >
+                <div className={styles.video__video} >
+                    <video src="https://res.cloudinary.com/dcds6y8vf/video/upload/v1753163547/VideoGallery/u22lb56hmgfi5ezq1pl3.mp4"
+                        controls
+                        className={styles.video__content} />
+                </div>
+                <div className={styles.video__description} >
+                    <h1 className={styles.video__heading} >
+                        Tobacco Leaves
+                    </h1>
+                    <h2 className={styles.video__subHeading} >
+                        Carefully Cultivated Leaves to later Provide surreal Experience.
+                    </h2>
+                </div>
+
+            </div >
+
+            {/* <div className={styles.video__item}> </div>
+        <div className={styles.video__item}> </div> */}
+
+        </div>
+    )
+}
--- app/(gallery)/videogallery/page.js
+++ app/(gallery)/videogallery/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroSection from './HeroSection.js'
+import VideoSection from './VideoSection.js'
 
 export default function VideoGallery
 () {
   return (
     <div>
       <HeroSection />
+      <VideoSection />
 
     </div>
   )
